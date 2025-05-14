e46c6b152e9e04a48ba98c6055912080c06a3a32
Gerard Ike <chinedumike85@gmail.com>
2025-05-13 21:07:34 -0400

    initial commit

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+const App = (props: Props) => {
+  return (
+    <div className='font-manrope'>
+      Hello World
+    </div>
+  )
+}
+
+export default App
